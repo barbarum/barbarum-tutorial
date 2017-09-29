b7e577cd4648deca1cad2c9c1d279a4d0e97d1fd
diwei <diwei@linkedin.com>
2017-09-29 15:41:40 +0800

    Add "Regular Expression Matching"

---
+++ src/test/java/com/barbarum/tutorial/code/TestRegularExpressionMatching.java
@@ -0,0 +1,27 @@
+package com.barbarum.tutorial.code;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
+public class TestRegularExpressionMatching extends BasicTestCase {
+
+    @Test
+    public void testIsMatch() throws Exception {
+        testTemplate(false, "aa", "a", RegularExpressionMatching::isMatch);
+        testTemplate(true, "aa", "aa", RegularExpressionMatching::isMatch);
+        testTemplate(false, "aaa", "aa", RegularExpressionMatching::isMatch);
+        testTemplate(true, "aa", "a*", RegularExpressionMatching::isMatch);
+        testTemplate(true, "aa", ".*", RegularExpressionMatching::isMatch);
+        testTemplate(true, "ab", ".*", RegularExpressionMatching::isMatch);
+        testTemplate(true, "aab", "c*a*b", RegularExpressionMatching::isMatch);
+        testTemplate(true, "a", "ab*", RegularExpressionMatching::isMatch);
+    }
+
+
+    private void testTemplate(boolean expected, String param1, String param2, BiFunction<String, String, Boolean> function) {
+        this.getUtil().testEquals(expected, param1, param2, Stream.of(function));
+    }
+}
